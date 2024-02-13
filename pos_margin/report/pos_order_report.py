f9820f0f7ce9154bbcc29f33d7c2dc947f215706
svenv <svenv@DESKTOP-VIFFEDB>
2024-02-13 08:57:56 +0300

    temp commented

--- pos_margin/report/pos_order_report.py
+++ pos_margin/report/pos_order_report.py
@@ -7,9 +7,9 @@ class PosOrderReport(models.Model):
     margin_total = fields.Float(string='Margin Total')
     margin_rate = fields.Float(string='Margin Rate', group_operator='avg')
 
-    def _select(self):
-        return super(PosOrderReport, self)._select() + """,
-    SUM(l.margin) as margin_total,
-    (SUM(l.margin / nullif(l.qty,0)) * 100 /
-    SUM(nullif(l.purchase_price,0)))::decimal as margin_rate
-    """
+    # def _select(self):
+    #     return super(PosOrderReport, self)._select() + """,
+    # SUM(l.margin) as margin_total,
+    # (SUM(l.margin / nullif(l.qty,0)) * 100 /
+    # SUM(nullif(l.purchase_price,0)))::decimal as margin_rate
+    # """
