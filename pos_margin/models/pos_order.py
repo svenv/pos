5613344b774610e8882a3209000f03b447c8afcf
svenv <svenv@DESKTOP-VIFFEDB>
2024-02-13 10:38:10 +0300

    temp commented

--- pos_margin/models/pos_order.py
+++ pos_margin/models/pos_order.py
@@ -11,7 +11,7 @@ class PosOrder(models.Model):
 
     # Columns Section
     margin = fields.Float(
-        'Margin', compute='_compute_margin', store=False,
+        'Margin', compute='_compute_margin', store=True,
         digits=dp.get_precision('Product Price'),
         help="It gives profitability by calculating the difference between"
         " the Unit Price and the cost price.")
